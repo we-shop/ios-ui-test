b647823843232df14dbc6ac41c7f7a8cb436b0cd
UANEMESIS <mike_zone@ukr.net>
2022-11-21 00:39:28 +0200

    ios test fixes 201122 part 17

--- ui_page_objects/post_page_object.py
+++ ui_page_objects/post_page_object.py
@@ -178,7 +178,7 @@ class PostPage:
 
 		# verify that edited text visible on next step
 		get_edited_question_banner_text = el_xpath(driver, QUESTION_EDITED_TEXT_MEDIA_TAB).text
-		assert get_edited_question_banner_text == f"Edited question {read_question_title}"
+		assert "edited" in get_edited_question_banner_text.lower()
 		click_on_next_btn_again = acc_id_click(driver, DONE_STEP_BTN_ADD_PRODUCT)
 
 		# next step
