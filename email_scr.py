d71be55eff799e8a46105c90ceb6f3ba9a966dc3
UANEMESIS <mike_zone@ukr.net>
2022-11-22 00:54:07 +0200

    ios fixes 21112022 part 43

--- email_scr.py
+++ email_scr.py
@@ -13,7 +13,7 @@ TRAVIS_EVENT_TYPE = os.getenv("TRAVIS_EVENT_TYPE")
 cur_date = datetime.today().strftime('%d-%m-%Y') # + "Test result:" + TEST_RESULT + " | " + TRAVIS_EVENT_TYPE
 
 
-recipients_list = ["abavabaraba@gmail.com", "mike_zone@ukr.net"] #, "danilo.lapegna@weshop.co.uk"]
+recipients_list = ["abavabaraba@gmail.com", "mike_zone@ukr.net", "danilo.lapegna@weshop.co.uk"]
 
 #recipients_list = ["abavabaraba@gmail.com", "mike_zone@ukr.net"] #, "sridevi.harsha@we.shop", "nik.lalev@weshop.co.uk", "smita.patil@weshop.co.uk", "shriharsha.ka@we.shop"]
 
