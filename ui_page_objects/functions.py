758216b702228db111aed7014daa5e6ff072db1f
UANEMESIS <mike_zone@ukr.net>
2022-11-22 18:06:48 +0200

    221122 ios from revert 5

--- ui_page_objects/functions.py
+++ ui_page_objects/functions.py
@@ -344,8 +344,8 @@ def long_click_xpath(driver, locator):
 	WebDriverWait(driver, 10).until(EC.presence_of_element_located((MobileBy.XPATH, locator)))
 	element = driver.find_element(MobileBy.XPATH, locator)
 	actions = TouchAction(driver)
-	actions.long_press(element, duration=2400)
-	actions.perform()
+	actions.long_press(element, duration=5400).perform()
+	#actions.perform()
 
 
 
