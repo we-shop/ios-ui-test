59301d42fcc881d704267e9163203fef97a22d77
UANEMESIS <mike_zone@ukr.net>
2022-12-27 00:07:21 +0200

    ios updates 261222 part 34

--- ui_page_objects/login_page_object.py
+++ ui_page_objects/login_page_object.py
@@ -422,7 +422,8 @@ class LoginPage:
 		click_on_start_shopping_btn = xpath_click(driver, NEXT_STEP_BTN)
 
 		# handle notification alert
-		handle_notification_alert_long(driver)
+		handle_notification_alert(driver)
+		#handle_notification_alert_long(driver)
 
 		# handling "weshop experience window"
 		try:
@@ -430,6 +431,9 @@ class LoginPage:
 		except:
 			time.sleep(1.2)
 			xpath_click(driver, WESHOP_EXPERIENCE_WIN_ALLOW_BTN)
+
+		# handling another one alert
+		handle_notification_alert(driver)
 
 		# after registration block
 		assert len(el_xpath(driver, LOGIN_TEXT_INSIDE_MODAL_SLIDE_WINDOW).text) > 5
@@ -445,7 +449,7 @@ class LoginPage:
 
 		click_on_start_shopping_btn_in_window = xpath_click(driver, LOGIN_MODAL_SLIDE_START_SHOP_BTN)
 
-		# qqq
+		# go to profile and check data
 		go_to_profile = acc_id_click(driver, FOOTER_ITEM_PROFILE)
 
 		time.sleep(3)
