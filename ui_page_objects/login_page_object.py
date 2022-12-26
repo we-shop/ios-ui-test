0a46cb5d25209e37ee1df31cc2ff09a6bf2905a2
UANEMESIS <mike_zone@ukr.net>
2022-12-26 10:46:22 +0200

    ios fixes 261222 part 4

--- ui_page_objects/login_page_object.py
+++ ui_page_objects/login_page_object.py
@@ -315,6 +315,10 @@ class LoginPage:
 		# registration fourth step
 		assert el_xpath(driver, NEXT_STEP_BTN).get_attribute("enabled") == "false"
 
+		click_on_mm_field = xpath_click(driver, REG_DATE_MM)
+		#time.sleep(1)
+		click_on_done_wheel_btn = acc_id_click(driver, REG_DATE_STEP_DONE_BTN)
+
 		click_on_dd_field = xpath_click(driver, REG_DATE_DD)
 		enter_date_dd = xpath_keys(driver, REG_DATE_DD, random_date_dd())
 
@@ -323,11 +327,6 @@ class LoginPage:
 
 		click_on_yyyy_field = xpath_click(driver, REG_DATE_YYYY)
 		enter_date_yyyy = xpath_keys(driver, REG_DATE_YYYY, random_date_yyyy())
-
-
-		click_on_mm_field = xpath_click(driver, REG_DATE_MM)
-		time.sleep(1)
-		click_on_done_wheel_btn = acc_id_click(driver, REG_DATE_STEP_DONE_BTN)
 
 		#REG_DATE_DD = "//XCUIElementTypeOther/XCUIElementTypeTextField[1]"
 		#REG_DATE_MM = "//XCUIElementTypeOther/XCUIElementTypeTextField[2]"
